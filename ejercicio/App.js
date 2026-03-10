433891e3f92b337cd062ab0dab71f2fbb55402e6
Alejandra <Alejandrapaulinocore@gmail.com>
2026-03-10 18:46:38 -0400

    guardado2

---
+++ ejercicio/App.js
@@ -0,0 +1,14 @@
+function saludar(){
+let msg = document.getElementById("saludar");
+    msg.innerHTML="HOLAAAAAAA";
+}
+
+function ocultar(){
+let msg = document.getElementById("saludar");
+    msg.innerHTML="";
+}
+
+function  color(){
+let color = document.getElementById("saludar");
+    color.style.color= "red";
+}
